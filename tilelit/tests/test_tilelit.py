6686a15dc686e5ebd7d7fcae762c73066ade5b2f
arokem <arokem@gmail.com>
2017-12-15 15:26:35 -0800

    Add test.

--- tilelit/tests/test_tilelit.py
+++ tilelit/tests/test_tilelit.py
@@ -4,6 +4,20 @@ import numpy as np
 import pandas as pd
 import numpy.testing as npt
 import tilelit as ti
+import skimage.data as skd
+from skimage import transform as tf
+
 
 def test_tile():
-    pass
+    static = skd.coins()
+    ty = np.random.randn() * 0.05 * static.shape[0]
+    tx = np.random.randn() * 0.05 * static.shape[1]
+    rot = np.random.randn() * np.pi/20
+    affine = tf.AffineTransform(rotation=rot, translation=(tx, ty))
+    moving = tf.warp(static, affine.inverse)
+    img_initial, aff_initial = \
+        ti.initial_alignment(static, moving, gaussian_blur=5)
+    img_final, aff_final = \
+        ti.fine_alignment(static, img_initial, starting_affine=aff_initial)
+
+    npt.assert_almost_equal(aff_final, affine.params)
