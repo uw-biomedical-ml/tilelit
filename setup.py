f244eb14856495f8d68d92500bfddc0747bb8281
arokem <arokem@gmail.com>
2017-12-15 15:33:23 -0800

    Fix up setup file.

--- setup.py
+++ setup.py
@@ -2,10 +2,18 @@ import os
 from setuptools import setup, find_packages
 PACKAGES = find_packages()
 
-# Get version and release info, which is all stored in shablona/version.py
-ver_file = os.path.join('shablona', 'version.py')
+# Get version and release info, which is all stored in tilelit/version.py
+ver_file = os.path.join('tilelit', 'version.py')
 with open(ver_file) as f:
     exec(f.read())
+
+REQUIRES = []
+with open('requirements.txt') as f:
+    l = f.readline()[:-1]
+    while l:
+        REQUIRES.append(l)
+        l = f.readline()[:-1]
+
 
 opts = dict(name=NAME,
             maintainer=MAINTAINER,
