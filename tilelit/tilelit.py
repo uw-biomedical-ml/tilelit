140c54ef934793c8bf4ac565dacc8ec2d7e2cc6c
Ubuntu <ubuntu@ip-172-31-8-242.us-west-2.compute.internal>
2018-07-05 22:25:56 +0000

    Adding more specific tools for histology registration

--- tilelit/tilelit.py
+++ tilelit/tilelit.py
@@ -1,4 +1,5 @@
 """tilelit : make coextensive tiles from massive images."""
+import os.path as op
 import numpy as np
 
 import skimage.io as sio
@@ -12,6 +13,8 @@ from dipy.align.imaffine import (MutualInformationMetric,
                                  AffineRegistration)
 
 from dipy.align.transforms import AffineTransform2D
+
+import tifffile
 
 
 def tiff_resize(fname, factor=10):
@@ -135,3 +138,61 @@ def fine_alignment(static, moving, starting_affine=None):
                             starting_affine=starting_affine)
     img_warp = affine.transform(moving)
     return img_warp, affine.affine
+
+
+def register_case(case, chan=2, factor=10):
+    """ """
+    he_file = '/home/ubuntu/data/%s_HE.tiff'%case
+    er_file = '/home/ubuntu/data/%s_ER.tiff'%case
+
+    im_he = tiff_resize(he_file, factor=factor)
+    im_stain = tiff_resize(er_file, factor=factor)
+    
+    img_stain_bw = im_stain[..., chan]
+    img_he_bw = im_he[..., chan]
+    
+    # Plot before registration
+    im1, im2 = plot_together(1-img_he_bw, 1-(img_stain_bw/img_stain_bw.max()))
+    # Register
+    img_final, aff_final = fine_alignment(img_stain_bw, img_he_bw, starting_affine=np.eye(3))
+    # Plot again after registration
+    new_image1, new_image2 = plot_together(1-(img_final/img_final.max()), 1-(img_stain_bw/img_stain_bw.max()))
+    
+    return aff_final
+
+
+def make_tiles(case, bucket, aff, box_size=(280, 280)):
+    im_stain_orig = tifffile.imread('/home/ubuntu/data/%s_ER.tiff'%case)
+    im_he_orig = tifffile.imread('/home/ubuntu/data/%s_HE.tiff'%case)
+    ii = 1
+    for y_corner in np.arange(0, im_stain_orig.shape[0], box_size[0]):
+        for x_corner in np.arange(0, im_stain_orig.shape[1], box_size[1]):
+            y_center = y_corner + box_size[0] // 2
+            x_center = x_corner + box_size[1] // 2
+            x_coords, y_coords = np.meshgrid(np.arange(x_center - box_size[1] // 2, x_center + box_size[1] // 2),
+                                             np.arange(y_center - box_size[0] // 2, y_center + box_size[0] // 2))
+
+            coords = np.array(list(zip(x_coords.ravel(), y_coords.ravel(), np.ones(np.product(box_size))))).astype(int)
+            if not (np.any(coords[:, 0] > im_stain_orig.shape[0]) or 
+                    np.any(coords[:, 1] > im_stain_orig.shape[1])):
+                im_stain_tile = im_stain_orig[(coords[:, 0], coords[:, 1])].reshape(box_size + (-1, ))
+                trans_coords = (np.round(np.dot(aff, coords.T))).T.astype(int)
+                if not (np.any(trans_coords < 0) or 
+                        np.any(trans_coords[:, 0] >= im_he_orig.shape[0]) or 
+                        np.any(trans_coords[:, 1] >= im_he_orig.shape[1])):
+                    im_he_tile = im_he_orig[(trans_coords[:, 0], trans_coords[:, 1])].reshape(box_size + (-1, ))
+                    mean_tile = im_he_tile[:, :, :3].mean()
+                    if mean_tile < 240:
+                        local_he_trans = './tile-he-%s.png'%case
+                        tifffile.imsave(local_he_trans, im_he_tile)                
+                        remote_he = op.join("BreastDeepLearning/RU-18-160-sections-8-12/TIFF/tiles/%s/HE/"%case,
+                                            'tile%08d.png' % ii)
+                        bucket.upload_file(local_he_trans, remote_he)
+                        local_stain ='./tile-er-%s.png'%case
+                        tifffile.imsave(local_stain, im_stain_tile)
+                        remote_stain = op.join("BreastDeepLearning/RU-18-160-sections-8-12/TIFF/tiles/%s/ER/"%case, 
+                                               'tile%08d.png' % ii)
+                        if not np.mod(ii, 50):
+                            print(ii)
+                        bucket.upload_file(local_stain, remote_stain)
+                        ii = ii + 1                    
